bfddf6a31dd49ebe35240962c3dc731b8b6c32be
Dhruv <haldardhruv@gmail.com>
2023-02-07 00:19:42 +0530

    update individual_company_stock.py

--- Code/individual_company_stock.py
+++ Code/individual_company_stock.py
@@ -1,8 +1,16 @@
 from io import StringIO
 import requests
 import pandas as pd
-from  datetime import datetime , timedelta
+import datetime
+from datetime import datetime , timedelta
 import bs4
+import tkinter as tk
+from tkinter import ttk
+from requests import Session
+
+"""
+    This function creates a GUI using the Tkinter library. The show_table function creates a window with a table that displays the data from the input dataframe. The input company symbol is passed to the getHistoryData function, which retrieves the stock data from NSE India's website and returns it as a dataframe. The returned dataframe is then passed to the show_table function, which displays the data in a table within a GUI window.
+    """
 
 session = requests.session()
 headers = {
@@ -13,7 +21,6 @@ head = {
                   "Chrome/87.0.4280.88 Safari/537.36 "
 }
 
-
 def getHistoryData(company, from_date=(datetime.today().strftime("%d-%m-%Y")), to_date=(datetime(datetime.today().year - 1, datetime.today().month,datetime.today().day).strftime("%d-%m-%Y"))):
     session.get("https://www.nseindia.com", headers=head)
     session.get("https://www.nseindia.com/get-quotes/equity?symbol=" + company, headers=head)  # to save cookies
@@ -23,8 +30,22 @@ def getHistoryData(company, from_date=(datetime.today().strftime("%d-%m-%Y")), t
     df = pd.read_csv(StringIO(webdata.text[3:]))
     return df
 
+def show_table(df):
+    root = tk.Tk()
+    frame = ttk.Frame(root)
+    frame.pack()
+    table = ttk.Treeview(frame, columns=df.columns)
+    table.pack()
 
+    for col in df.columns:
+        table.heading(col, text=col.capitalize())
+        table.column(col, width=100, anchor="center")
+        table['show'] = 'headings'
 
+    for i, row in df.iterrows():
+        table.insert("", "end", values=list(row))
+
+    root.mainloop()
 
 def niftyHistoryData(varient, from_date = ((datetime(datetime.today().year - 1, datetime.today().month, datetime.today().day) + timedelta(days=2)).strftime("%d-%m-%Y")), to_date =(datetime.today().strftime("%d-%m-%Y"))):
     varient = varient.upper()
@@ -37,6 +58,8 @@ def niftyHistoryData(varient, from_date = ((datetime(datetime.today().year - 1, 
     soup = bs4.BeautifulSoup(webData.text, 'html5lib')
     return pd.read_csv(StringIO(soup.find('div', {'id': 'csvContentDiv'}).contents[0].replace(':','\n')))
 
-
-# print(getHistoryData('SHREECEM',from_date='30-04-2020',to_date='30-04-2021'))
+if __name__ == "__main__":
+    df = getHistoryData('SHREECEM',from_date='30-04-2020',to_date='30-04-2021')
+    show_table(df)
+#print(getHistoryData('SHREECEM',from_date='30-04-2020',to_date='30-04-2021'))
 # print(niftyHistoryData('NIFTY 50'))
