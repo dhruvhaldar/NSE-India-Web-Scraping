4c0f9cc308f53ec0389f2bd7480742fc699b40cf
dhruvhaldar <haldardhruv@gmail.com>
2023-02-25 17:33:53 +0530

    xsxa

--- Codes/news_aggregator.py
+++ Codes/news_aggregator.py
@@ -1,20 +1,38 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-url = "https://www.google.com/search?q=AAPL+stock+news&sxsrf=AJOqlzWU-WeFAZeiYAn7rnDHctmFtfkZOA:1677320347545&source=lnms&tbm=nws&sa=X&ved=2ahUKEwj5rfWbubD9AhXT9XMBHdKPDBkQ_AUoAXoECAEQAw&biw=1384&bih=778&dpr=1.25"
+def scrape_news(ticker, date_range):
+    # Construct the URL
+    url = f"https://www.google.com/search?q={ticker}+stock+news&tbm=nws&tbs=cdr:1,cd_min:{date_range[0]},cd_max:{date_range[1]}"
+    #print("URL:", url)
+    #https://www.google.com/search?q=AAPL+stock+news&tbm=nws&tbs=cdr:1,cd_min:2023-02-18,cd_max:2023-02-25
+    
+    # Send a GET request to the URL and get the HTML content
+    response = requests.get(url)
+    #print("Response:", response)
 
-# Send a GET request to the URL and get the HTML content
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, 'html5lib')
 
-# Find all the news article titles and links
-news_articles = soup.find_all('div', {'class': 'dbsr'})
+    heading_object=soup.find_all( 'h3' )
+    #print(heading_object)
+    
+    # Iterate through the object 
+    # and print it as a string.
+    for info in heading_object:
+        print(info.getText())
+        print("------")
+    
+    """ # Find all the news article titles and links
+    news_articles = soup.find_all('div', {'class': 'dbsr'})
+    print("Found", len(news_articles), "news articles")
 
-# Print each article's title and link
-for article in news_articles:
-    title = article.find('div', {'class': 'JheGif nDgy9d'}).get_text()
-    link = article.find('a')['href']
-    print(title)
-    print(link)
-    #print("Title: {title}")
-    #print("Link: {link}\n")
+    # Print each article's title and link
+    for article in news_articles:
+        title = article.find('div', {'class': 'JheGif nDgy9d'}).get_text()
+        link = article.find('a')['href']
+        print(f"Title: {title}")
+        print(f"Link: {link}\n") """
+
+ticker = "AAPL"
+date_range = ["2022-01-01", "2022-02-28"]        
+scrape_news(ticker, date_range)
